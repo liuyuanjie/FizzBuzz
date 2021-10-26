5e0146db4680aaa139c89e5a606c67990f5b589f
yuanjie.liu <kevinliuxian@outlook.com>
2021-10-26 15:40:31 +0800

    finish feature

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -1,12 +1,38 @@
+using System;
+
 namespace FizzBuzz
 {
     public static class FizzBuzzGame
     {
+        private const int FizzNumber = 3;
+        private const int BuzzNumber = 5;
+        private const int WhizzNumber = 7;
+
         public static string Report(int order)
         {
-            if (order == 3) return "Fizz";
+            if (order.ToString().Contains(FizzNumber.ToString())) return "Fizz";
 
-            return null;
+            var result = string.Empty;
+            if (IsFizz(order)) result += "Fizz";
+            if (IsBuzz(order)) result += "Buzz";
+            if (IsWhizz(order)) result += "Whizz";
+
+            return string.IsNullOrEmpty(result) ? order.ToString() : result;
+        }
+
+        private static bool IsWhizz(int order)
+        {
+            return order % WhizzNumber == 0;
+        }
+
+        private static bool IsBuzz(int order)
+        {
+            return order % BuzzNumber == 0;
+        }
+
+        private static bool IsFizz(int order)
+        {
+            return order % FizzNumber == 0;
         }
     }
 }
--- FizzBuzzTests/FizzBuzzGameTests.cs
+++ FizzBuzzTests/FizzBuzzGameTests.cs
@@ -8,10 +8,73 @@ namespace FizzBuzzTests
         [Fact]
         public void Should_Report_Fizz_When_Reporting_Given_Contains_3()
         {
-            var order = 3;
+            var order = 13;
             var report = FizzBuzzGame.Report(order);
             Assert.Equal("Fizz", report);
-            
+        }
+
+        [Fact]
+        public void Should_Report_Fizz_When_Reporting_Given_Divide_3()
+        {
+            var order = 6;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("Fizz", report);
+        }
+        
+        [Fact]
+        public void Should_Report_Buzz_When_Reporting_Given_Divide_5()
+        {
+            var order = 10;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("Buzz", report);
+        }
+        
+        [Fact]
+        public void Should_Report_Whizz_When_Reporting_Given_Divide_7()
+        {
+            var order = 14;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("Whizz", report);
+        }
+
+        [Fact]
+        public void Should_Report_FizzBuzz_When_Reporting_Given_Divide_3_And_5()
+        {
+            var order = 15;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("FizzBuzz", report);
+        }
+
+        [Fact]
+        public void Should_Report_FizzWhizz_When_Reporting_Given_Divide_3_And_7()
+        {
+            var order = 21;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("FizzWhizz", report);
+        }
+        
+        [Fact]
+        public void Should_Report_BuzzWhizz_When_Reporting_Given_Divide_5_And_7()
+        {
+            var order = 70;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("BuzzWhizz", report);
+        }
+        
+        [Fact]
+        public void Should_Report_FizzBuzzWhizz_When_Reporting_Given_Divide_3_And_5_And_7()
+        {
+            var order = 210;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal("FizzBuzzWhizz", report);
+        }
+        
+        [Fact]
+        public void Should_Report_Input_When_Reporting_Given_Can_Not_Divide_3_Or_5_Or_7()
+        {
+            var order = 22;
+            var report = FizzBuzzGame.Report(order);
+            Assert.Equal(order.ToString(), report);
         }
     }
 }
